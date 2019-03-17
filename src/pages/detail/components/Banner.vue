4c3fb960cd847c52351386567d3bc5ca073798d4
yeshuijin <yeshuijin@outlook.com>
2019-03-17 23:37:39 +0800

    banner-info

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1411/31/74eabd4dc95afd494d92a5006c9ad4f5.water.jpg_600x330_f1cf8f19.jpg" />
+    <div class="banner-info">
+      <div class="banner-title">美林湖温泉</div>
+      <div class="banner-num"><span class="iconfont banner-icon">&#xe692;</span>42</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    position: relative
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-num
+        height: .32rem
+        line-height: .32rem
+        margin-top: .14rem
+        padding: 0 .4rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
