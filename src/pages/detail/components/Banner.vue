4276815575f4abeba9cd9dc7f99cbfdb19440859
yeshuijin <yeshuijin@outlook.com>
2019-03-18 00:30:19 +0800

    banner-finish

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,16 +1,41 @@
 <template>
-  <div class="banner">
-    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1411/31/74eabd4dc95afd494d92a5006c9ad4f5.water.jpg_600x330_f1cf8f19.jpg" />
-    <div class="banner-info">
-      <div class="banner-title">美林湖温泉</div>
-      <div class="banner-num"><span class="iconfont banner-icon">&#xe692;</span>42</div>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1411/31/74eabd4dc95afd494d92a5006c9ad4f5.water.jpg_600x330_f1cf8f19.jpg" />
+      <div class="banner-info">
+        <div class="banner-title">美林湖温泉</div>
+        <div class="banner-num"><span class="iconfont banner-icon">&#xe692;</span>42</div>
+      </div>
     </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
   </div>
 </template>
 
 <script>
+import CommonGallary from '../../../common/gallary/Gallary'
 export default {
-  name: 'DetailBanner'
+  name: 'DetailBanner',
+  components: {
+    CommonGallary
+  },
+  data () {
+    return {
+      showGallary: false,
+      imgs: [
+        'http://img1.qunarzz.com/sight/p0/1411/8f/6326f9b589ee83200013133ce619428e.water.jpg_r_800x800_540144e3.jpg',
+        'http://img1.qunarzz.com/sight/p0/1511/f3/f329d2814e97208190.water.jpg_r_800x800_5730804b.jpg',
+        'http://img1.qunarzz.com/sight/p0/201307/19/3eb6b4a74f64f282c8d65eac.jpg_r_800x800_977b835d.jpg'
+      ]
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  }
 }
 </script>
 
