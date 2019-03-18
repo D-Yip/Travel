a140df4b8ab35d13a382f925aeb1a41493265e64
yeshuijin <yeshuijin@outlook.com>
2019-03-18 22:44:40 +0800

    header-animation

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -26,8 +26,12 @@ export default {
   activated () {
     window.addEventListener('scroll', this.handleScroll)
   },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
   methods: {
     handleScroll () {
+      console.log('scroll')
       const top = document.documentElement.scrollTop
       if (top > 60) {
         let opacity = top / 140
